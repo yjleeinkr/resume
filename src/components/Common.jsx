0437bebb65f055b67c623598f3c3e3e3d455be24
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-07 04:11:17 +0900

    add common styles

--- src/components/Common.jsx
+++ src/components/Common.jsx
@@ -10,8 +10,14 @@ export const SubTitle = styled.h2`
   font-size : 1.8rem;
   font-weight: 600;
 `
-
 export const Subject = styled.h3`
-  font-size : 1.5rem;
+  font-size: 1.5rem;
   font-weight: 600;
-`
+  padding: 5px 0px;
+`;
+
+export const DetailSubject = styled.h3`
+  font-size: 1.2rem;
+  font-weight: 600;
+  padding: 5px 0px;
+`;
