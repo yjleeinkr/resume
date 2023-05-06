a8cf6fd4d2f03f58c94c059d2046b0cddbd22b5a
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-07 02:42:05 +0900

    common styled components

---
+++ src/components/Common.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size : 2.5rem;
+  font-weight: 500;
+  padding: 5px 0px;
+`
+
+export const SubTitle = styled.h2`
+  font-size : 1.8rem;
+  font-weight: 600;
+`
+
+export const Subject = styled.h3`
+  font-size : 1.5rem;
+  font-weight: 600;
+`
