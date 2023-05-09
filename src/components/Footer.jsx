cdd814b5aea603ccd204b29d31ef906b1397df47
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-09 16:11:13 +0900

    style: add padding more

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+
+const FooterLayout = styled.footer`
+  text-align: right;
+  padding: 100px 0px 10px 0px;
+  color: #000;
+  font-weight: 300;
+`;
+
 function Footer() {
-  return <div style={{ textAlign: "right" }}>ⓒ 2023 YeonJeong Lee</div>;
+  return <FooterLayout>ⓒ 2023 YeonJeong Lee</FooterLayout>;
 }
 
 export default Footer;
